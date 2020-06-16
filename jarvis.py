c9c9690ba1364a18f28472382922f6aec3540d77
RamKumar323 <singamsetty.ramkumar10@gmail.com>
2020-06-16 13:58:18 +0530

    Date Function

--- jarvis.py
+++ jarvis.py
@@ -13,4 +13,12 @@ def time():
     Time = datetime.datetime.now().strftime("%H:%M:%S")
     speak(Time)
 
-time()
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+    speak(date)
+    speak(month)
+    speak(year)
+
+date()
